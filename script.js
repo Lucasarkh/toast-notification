4bbed5b611b4739a41ccc033e866d29a442e500f
Lucasarkh <lucasarchanjo1010@gmail.com>
2023-03-25 12:44:09 -0300

    fix: fixed issue with toasts when overlapping

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 let x;
-let y;
 let toastSuccess = document.getElementById("toast--success");
 let toastError = document.getElementById("toast--error");
 let containerSuccess = document.getElementsByClassName("container-toast-success")[0];
 let containerError = document.getElementsByClassName("container-toast-error")[0];
 
-
+let successMsgActive = false;
+let errorMsgActive = false;
 
 //Exibe o toast de sucesso
 function showSuccessMsg () {
-    clearTimeout(x);
-    if (toastSuccess.classList.contains("hide-toast-success")) {
-        containerSuccess.style.zIndex = "2";
-        containerError.style.zIndex = "1";
-        toastSuccess.classList.remove("hide-toast-success");
-    }
-    
-    toastSuccess.classList.add("show-toast-success");
+    // Verifica se a função showErrorMsg não está ativa
+    if (!errorMsgActive) {
+        successMsgActive = true;
+        clearTimeout(x);
+        if (toastSuccess.classList.contains("hide-toast-success")) {
+            containerSuccess.style.zIndex = "2";
+            containerError.style.zIndex = "1";
+            toastSuccess.classList.remove("hide-toast-success");
+        }
+        
+        toastSuccess.classList.add("show-toast-success");
 
-    //Define um tempo para o toast de sucesso desaparecer
-    x = setTimeout(()=> {
-        toastSuccess.classList.remove("show-toast-success");
-        toastSuccess.classList.add("hide-toast-success");
-        toastSuccess.style.transition = "transform 1s ease-in-out";
-    },4000);
+        //Define um tempo para o toast de sucesso desaparecer
+        x = setTimeout(()=> {
+            toastSuccess.classList.remove("show-toast-success");
+            toastSuccess.classList.add("hide-toast-success");
+            toastSuccess.style.transition = "transform 1s ease-in-out";
+            successMsgActive = false;
+        },4000);
+    }
 }
 
 //Fecha o toast de sucesso com um click
@@ -35,26 +40,33 @@ function closeToastSuccess() {
     toastSuccess.style.transition = "transform 1s ease-in-out";
     containerError.style.zIndex = "2";
     containerSuccess.style.zIndex = "1";
-
+    successMsgActive = false;
 }
+
+
 
 //Exibe o toast de Erro
 function showErrorMsg () {
-    clearTimeout(x);
-    if (toastError.classList.contains("hide-toast-error")) {
-        containerError.style.zIndex = "2";
-        containerSuccess.style.zIndex = "1";
-        toastError.classList.remove("hide-toast-error");
-    }
-    
-    toastError.classList.add("show-toast-error");
+    // Verifica se a função showSuccessMsg não está ativa
+    if (!successMsgActive) {
+        errorMsgActive = true;
+        clearTimeout(x);
+        if (toastError.classList.contains("hide-toast-error")) {
+            containerError.style.zIndex = "2";
+            containerSuccess.style.zIndex = "1";
+            toastError.classList.remove("hide-toast-error");
+        }
+        
+        toastError.classList.add("show-toast-error");
 
-    //Define um tempo para o toast de erro desaparecer
-    x = setTimeout(()=> {
-        toastError.classList.remove("show-toast-error");
-        toastError.classList.add("hide-toast-error");
-        toastError.style.transition = "transform 1s ease-in-out";
-    },4000);
+        //Define um tempo para o toast de erro desaparecer
+        x = setTimeout(()=> {
+            toastError.classList.remove("show-toast-error");
+            toastError.classList.add("hide-toast-error");
+            toastError.style.transition = "transform 1s ease-in-out";
+            errorMsgActive = false;
+        },4000);
+    }
 }
 
 //Fecha o toast de erro com um click
@@ -66,14 +78,5 @@ function closeToastError() {
     toastError.style.transition = "transform 1s ease-in-out";
     containerError.style.zIndex = "1";
     containerSuccess.style.zIndex = "2";
-
+    errorMsgActive = false;
 }
-
-
-
-
-
-
-
-
-
